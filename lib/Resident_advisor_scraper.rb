1151bdf689d2f49e1c5ce8727f6b33d0d3743694
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2016-08-30 10:15:26 +0100

    scraper for id's done

--- lib/Resident_advisor_scraper.rb
+++ lib/Resident_advisor_scraper.rb
@@ -1,9 +1,14 @@
 require 'open-uri'
 require 'nokogiri'
 
-event =
+current_year = Date.today.year
+# p current_year
+current_month = Date.today.month
+# p current_month
+current_day = Date.today.day
+# p current_day
 
-html_file = open("https://www.residentadvisor.net/events.aspx?ai=13&v=month&mn=8&yr=2016&dy=29")
+html_file = open("https://www.residentadvisor.net/events.aspx?ai=13&v=month&mn=#{current_month}&yr=#{current_year}&dy=#{current_day}")
 html_doc = Nokogiri::HTML(html_file)
 
 # html_doc.search('.bbox').each do |element|
@@ -14,8 +19,15 @@ id_s = []
 
 html_doc.search('.bbox').each do |link|
 
-  id = link.at("h1 a[href]")
-  id_s << id
+  id = link.at("h1 a[href]").to_s
+  # id.gsub(/\D/, "")
+  id_s << id.scan(/(\d\d\d\d\d\d)/)
+  # p id
 end
+id_s.delete_if do |id|
+  id.empty?
+end
+
+id_s.flatten!
 
 p id_s

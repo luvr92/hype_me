56a3f6365b70510f4730884230ddeb2f0dff487e
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2016-08-31 16:50:46 +0100

    geolocator for user working

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,9 @@
 class EventsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
+  def index
+
+  end
 
   def show
 

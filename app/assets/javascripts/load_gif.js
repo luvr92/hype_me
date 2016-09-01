a52c09eb4fca257dbac45a358047fdb05b6b69f3
Jordy Backes <backes.jordy@gmail.com>
2016-09-01 15:54:47 +0100

    Js

---
+++ app/assets/javascripts/load_gif.js
@@ -0,0 +1,9 @@
+  // $(window).load(function() {
+  //   // Animate loader off screen
+  //   $(".se-pre-con").fadeOut("slow");;
+  // });
+
+
+  $('hype-btn').on('click', function(event) {
+    $('.se-pre-con').removeClass('hidden');
+  });

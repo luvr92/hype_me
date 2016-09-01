68c7e58a060151887ca9f86e06ee4a9218613f50
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2016-09-01 16:56:43 +0100

    scraper actually done i promise

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,11 @@ class Event < ApplicationRecord
   belongs_to :club
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
+  has_many :event_artists
+  has_many :artists, through: :event_artists
 end
+
+# artist = Artist.find?
+# artist = Artist.new
+# event.event_artists.build(artist: artist)
+# event.save

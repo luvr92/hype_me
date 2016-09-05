ba190f130d19344c544f3c2f5e1b20e644fc85b7
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2016-09-05 11:20:54 +0100

    next/prev event jquery done but no swipe

--- app/assets/javascripts/event_switcher.js
+++ app/assets/javascripts/event_switcher.js
@@ -1,19 +1,24 @@
-$('#next-event-button').click(function() {
-  nextEvent();
+$(document).ready(function() {
+  $('#next-event-button').click(function() {
+    nextEvent();
+  });
+
+  $('#prev-event-button').click(function() {
+    previousEvent();
+  });
+
 });
 
-$('#prev-event-button').click(function() {
-  previousEvent();
-});
 
 
 
 
 
 function nextEvent() {
+  console.log("next");
   $('.active-event').removeClass("active-event").addClass("non-active-event").next().removeClass("non-active-event").addClass("active-event");
 };
 
 function previousEvent() {
-  $('.active-event').removeClass("active-event").addClass("non-active-event").next().removeClass("non-active-event").addClass("active-event");
+  $('.active-event').removeClass("active-event").addClass("non-active-event").prev().removeClass("non-active-event").addClass("active-event");
 };

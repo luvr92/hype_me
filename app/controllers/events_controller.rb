dfd31af8443eda8051902e60e129377a2f8db288
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2016-09-09 10:33:42 +0100

    tweakings

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,9 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.near([params[:lat], params[:lng]], 50).where(date: Date.today).first(10)
+    if @events.empty?
+      @events = Event.near([params[:lat], params[:lng]], 50).where(date: Date.today.prev_day).first(10)
+    end
     @user_lat = params[:lat]
     @user_lng = params[:lng]
   end
